a0d10552f94cbed97179c7eaa804722c60d5a5cf
Sanket <singhsanket143@gmail.com>
2017-07-15 02:25:48 +0530

    Added Creating Tweet Functionality Using Scaffold

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var textarea = document.getElementById('tweet_content');
+  console.log(textarea);
+  var char = document.getElementById('characters');
+  var create_tweet_submit = document.getElementById('create_tweet_submit');
+  console.log(char);
+  textarea.addEventListener('input', function() {
+        console.log(textarea.value.length);
+
+
+        char.innerHTML = textarea.value.length;
+
+          if (textarea.value.length > 140) {
+            char.style.color = "red";
+            create_tweet_submit.disabled = true;
+
+
+        } else {
+            char.style.color = "black";
+            create_tweet_submit.disabled = false;
+
+        }
+    });
+
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714160754) do
+ActiveRecord::Schema.define(version: 20170714201553) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
